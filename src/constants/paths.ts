8addbdf315c60ebc9b627813b068ed01421559d2
Jeonhayeon0 <jhayeon0623@hufs.ac.kr>
2025-08-05 21:41:14 +0900

    생년월일 입력 페이지 추가

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -5,6 +5,7 @@
 export const PATHS = {
   LANDING: '/',       // 랜딩 페이지
   MAIN: '/main',      // 메인 페이지
-  INPUT: '/input',
+  BIRTH: '/birth',    // 생년월일 입력 페이지
+  INPUT: '/input',    // 질문 페이지
   RESULT: '/result',
 } as const;
