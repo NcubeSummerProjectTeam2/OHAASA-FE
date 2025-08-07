d226431b132b9bb063732c75b30cfb2714b42129
Jeonhayeon0 <jhayeon0623@hufs.ac.kr>
2025-08-07 17:20:24 +0900

    환경변수 설정 및 fetch 주소 수정

--- src/api/getHoroscope.ts
+++ src/api/getHoroscope.ts
@@ -14,7 +14,13 @@ const zodiacToNum:{[key:string]:string}={
 };
 
 export async function getTodayHoroscope(zodiac:string){
-    const response = await fetch(`http://localhost:4000/api/horoscope?zodiac=${zodiac}`);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/result`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
     const data = await response.json();
     const userHoroscope=data.horoscope;
     console.log('백엔드 응답 data : ', data);
